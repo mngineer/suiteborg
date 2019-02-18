a967ea6ef28fe21033e3685c7b5d441e5f526c77
mngineer <ryanabloom@aol.com>
2019-02-18 11:21:58 -0600

    Fixed local scripts for WiT demo

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -4,4 +4,8 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "SuiteBorg" %* 
+REM Remove interactivity of these two lines for deployment
+SET /P BOT_NAME=Bot Name:
+SET /P HUBOT_SLACK_TOKEN=Slack Bot Token:
+
+node_modules\.bin\hubot.cmd --name "%BOT_NAME%" %* 
