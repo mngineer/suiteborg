35cf093f5f67fb646ce57fac7887cc7154a3ddd2
mngineer <ryanabloom@aol.com>
2019-06-18 10:59:45 -0500

    Made deployable again; made special interactive versions of bot launch scripts

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -4,8 +4,4 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-REM Remove interactivity of these two lines for deployment
-SET /P BOT_NAME=Bot Name:
-SET /P HUBOT_SLACK_TOKEN=Slack Bot Token:
-
-node_modules\.bin\hubot.cmd --name "%BOT_NAME%" %* 
+node_modules\.bin\hubot.cmd --name "SuiteBorg" %* 
